5e76e681dd30b51da46ebb0a57d4a581b74c4c5f
alirezaasm <alirezaasm1999@gmail.com>
2020-11-08 10:05:58 +0330

    Add Schiff Register inputs that provide output of of full-adder inputs

---
+++ Counter.v
@@ -0,0 +1,14 @@
+`include "../FullAdder64/FullAdder64bit.v"
+`include "../shift_register/SHIFT_REGISTER.v"
+
+module counter (s1,clk,s0,d)
+
+    input s1;
+    input s0;
+    input clk;
+
+
+
+
+
+endmodule
