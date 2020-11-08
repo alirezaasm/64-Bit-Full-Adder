d37469dd8d55fcf951e6b98f863c9fd65ef6e84a
alirezaasm <alirezaasm1999@gmail.com>
2020-11-08 12:51:31 +0330

    parallel load

--- counter/Counter_tb.v
+++ counter/Counter_tb.v
@@ -7,3 +7,13 @@ module usv_tb;
     wire [63:0]q; 
 	
 	    counter(.s1(s1) , clk ,.s0(s0),.d(d))
+
+
+	initial begin
+
+    // parallel load
+    d = 64'b0;
+    s1 = 1'b1;
+    s0 = 1'b1;
+    
+    end
