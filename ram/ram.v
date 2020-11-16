ace00c35f11f15bc06151c1c552f9ee87c086c5b
Mason <alirezaasm1999@gmail.com>
2020-11-17 00:47:37 +0330

    complete

--- ram/ram.v
+++ ram/ram.v
@@ -1,18 +1,16 @@
-module ram (Data,
-            address,
-            data_out
-            );
+module ram (Data,address,data_out);
 		
     parameter address_size = 64;
     parameter data_size    = 32;
 	
-	 input reg [address_size-1:0] Data [data_size-1 : 0];
+	
+	input reg [address_size-1:0] Data [data_size-1 : 0];
 		
 		
-	 always @(*)
-    begin
-        data_out = Data[address]
-    end
+			always @(*)
+			begin
+				data_out = Data[address]
+			end
 	
 
 endmodule
