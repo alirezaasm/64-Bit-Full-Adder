a9abe8be459fae1d65ed3f1d155f6bffba6a47f1
Mason <alirezaasm1999@gmail.com>
2020-11-17 00:57:15 +0330

    Using expanded input functions and using counter output values in RAM

--- ram/ram_tb.v
+++ ram/ram_tb.v
@@ -8,6 +8,9 @@ module ram_tb;
 	
 	reg Data; 
     output wire data_out;
+		
+		counter counter (.clk(clk) , .reset(reset),.s(s),.data_in(.data_in),.SISR(SISR),.SISL(SISL),.data_out(data_out))
+    ram ram(.Data(Data), .address(data_out) , .data_out(reg_data)
 
 
 
