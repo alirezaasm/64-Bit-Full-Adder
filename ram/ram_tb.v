bf16103af34eed172ca45da445e93beb459fdd3b
Mason <alirezaasm1999@gmail.com>
2020-11-17 00:59:42 +0330

    Testing the input values of data in the benchmark test in the built-in presentations

--- ram/ram_tb.v
+++ ram/ram_tb.v
@@ -12,6 +12,19 @@ module ram_tb;
 		counter counter (.clk(clk) , .reset(reset),.s(s),.data_in(.data_in),.SISR(SISR),.SISL(SISL),.data_out(data_out))
     ram ram(.Data(Data), .address(data_out) , .data_out(reg_data)
 
+		
+		initial begin
+        Data[0]  = $random%10;
+        Data[4]  = $random%10;
+        Data[8]  = $random%10;
+        Data[12] = $random%10;
+        reset    = 0;
+        s        = b'11;
+        $display("reg: %d", data_out)
+        #10 $display("reg: %d", data_out)
+        #10 $display("reg: %d", data_out)
+        #10 $display("reg: %d", data_out)
+    end
 
 
 
