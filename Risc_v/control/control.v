060ad799723db8e924ac3ff4c077fad2ac785792
Mason <alirezaasm1999@gmail.com>
2020-12-29 00:01:24 +0330

    prepare output

--- Risc_v/control/control.v
+++ Risc_v/control/control.v
@@ -1,3 +1,5 @@
 module control (op_code, reg_dst , alu_src, mem_to_reg , reg_write , mem_read , mem_write, branch , alu_op)
 
-		    input[6:0] op_code;
+		     input[6:0] op_code;
+		     output reg_dst , alu_src, mem_to_reg , reg_write , mem_read , mem_write, branch;
+             output [1:0] alu_op;
